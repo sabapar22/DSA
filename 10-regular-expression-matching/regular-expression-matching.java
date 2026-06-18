f559d7c513f74e31574f5e5abb722f8a4ae403d2
Saba Parveen <sabasabir2206@gmail.com>
2026-06-18 22:05:58 +0530

    Time: 2 ms (49.94%) | Memory: 43 MB (96.80%) - LeetSync

---
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int m=s.length();
+        int n=p.length();
+
+        boolean[][] dp=new boolean[m+1][n+1];
+        dp[m][n]=true;
+
+        for(int j=n-1;j>=0; j--){
+            if(j+1<n && p.charAt(j+1)=='*'){
+                dp[m][j]=dp[m][j+2];
+            }
+            else{
+                dp[m][j]=false;
+            }
+        }
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                boolean firstMatch=(p.charAt(j)== s.charAt(i) || p.charAt(j)=='.' );
+                if(j+1<n && p.charAt(j+1)=='*'){
+                    dp[i][j]=dp[i][j+2] || (firstMatch && dp[i+1][j]);
+
+                }else{
+                    dp[i][j]= firstMatch && dp[i+1][j+1];
+                }
+            }
+        }
+        return dp[0][0];
+        
+    }
+}
