2aaf603bac1c5f73a337d01a5d4c9a89617bb607
Saba Parveen <sabasabir2206@gmail.com>
2026-06-29 22:55:37 +0530

    Time: 1 ms (99.92%) | Memory: 46.9 MB (45.36%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -5,28 +5,54 @@
  *     ListNode next;
  *     ListNode() {}
  *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ *     ListNode(int val, ListNode next) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
  * }
  */
+
 class Solution {
+
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists==null || lists.length==0) return null;
+        if (lists == null || lists.length == 0)
+            return null;
 
-        ListNode head=new ListNode(0);
-        ListNode temp=head;
-        List<Integer> l= new ArrayList<>();
-        for(ListNode List:lists){
-            while(List!=null){
-                l.add(List.val);
-                List=List.next;
+        return divide(lists, 0, lists.length - 1);
+    }
 
+    private ListNode divide(ListNode[] lists, int left, int right) {
+        if (left == right)
+            return lists[left];
+
+        int mid = left + (right - left) / 2;
+
+        ListNode l1 = divide(lists, left, mid);
+        ListNode l2 = divide(lists, mid + 1, right);
+
+        return merge(l1, l2);
+    }
+
+    private ListNode merge(ListNode l1, ListNode l2) {
+
+        ListNode dummy = new ListNode(-1);
+        ListNode tail = dummy;
+
+        while (l1 != null && l2 != null) {
+
+            if (l1.val <= l2.val) {
+                tail.next = l1;
+                l1 = l1.next;
+            } else {
+                tail.next = l2;
+                l2 = l2.next;
             }
+
+            tail = tail.next;
         }
-        Collections.sort(l);
-        for(int val:l){
-            temp.next=new ListNode(val);
-            temp=temp.next;
-        }
-        return head.next;
+
+        tail.next = (l1 != null) ? l1 : l2;
+
+        return dummy.next;
     }
 }
