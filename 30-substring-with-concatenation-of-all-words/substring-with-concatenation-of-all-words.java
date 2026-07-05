94e36dada1116325ac30ba55f2a8913da6bfc3c0
Saba Parveen <sabasabir2206@gmail.com>
2026-07-05 21:22:18 +0530

    Time: 11 ms (98.92%) | Memory: 47.6 MB (13.25%) - LeetSync

---
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
@@ -0,0 +1,53 @@
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> result = new ArrayList<>();
+
+        if (s == null || s.length() == 0 || words == null || words.length == 0)
+            return result;
+
+        Map<String, Integer> wordCount = new HashMap<>();
+        for (String word : words)
+            wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+
+        int wordLen = words[0].length();
+        int totalWords = words.length;
+        int windowLen = wordLen * totalWords;
+
+        for (int offset = 0; offset < wordLen; offset++) {
+            int left = offset;
+            int count = 0;
+            Map<String, Integer> window = new HashMap<>();
+
+            for (int right = offset; right + wordLen <= s.length(); right += wordLen) {
+                String word = s.substring(right, right + wordLen);
+
+                if (wordCount.containsKey(word)) {
+                    window.put(word, window.getOrDefault(word, 0) + 1);
+                    count++;
+
+                    while (window.get(word) > wordCount.get(word)) {
+                        String leftWord = s.substring(left, left + wordLen);
+                        window.put(leftWord, window.get(leftWord) - 1);
+                        left += wordLen;
+                        count--;
+                    }
+
+                    if (count == totalWords) {
+                        result.add(left);
+
+                        String leftWord = s.substring(left, left + wordLen);
+                        window.put(leftWord, window.get(leftWord) - 1);
+                        left += wordLen;
+                        count--;
+                    }
+                } else {
+                    window.clear();
+                    count = 0;
+                    left = right + wordLen;
+                }
+            }
+        }
+
+        return result;
+    }
+}
