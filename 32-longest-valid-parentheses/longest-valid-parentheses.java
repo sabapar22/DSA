3a17fc14580f39110cb9592135e5f6c62df57767
Saba Parveen <sabasabir2206@gmail.com>
2026-07-07 17:28:43 +0530

    Time: 5 ms (82.64%) | Memory: 46.6 MB (26.32%) - LeetSync

---
+++ 32-longest-valid-parentheses/longest-valid-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int longestValidParentheses(String s) {
+        int maxlength =0;
+        Stack<Integer> stack=new Stack<>();
+        stack.push(-1);
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('){
+                stack.push(i);
+            }
+            else{
+                stack.pop();
+                if(stack.isEmpty()){
+                    stack.push(i);
+                }
+                else{
+                    maxlength=Math.max(maxlength,i-stack.peek());
+                }
+            }
+        }
+        return maxlength;
+
+    }
+}
